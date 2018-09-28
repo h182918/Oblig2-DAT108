31c82ebd2aec07cd690355c9f58aa07822a0a2eb
Helene <helene-nordal@hotmail.com>
2018-09-28 14:59:08 +0200

    oppdat2

--- src/no/hvl/dat108/handlelisteServlet.java
+++ src/no/hvl/dat108/handlelisteServlet.java
@@ -14,33 +14,46 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/handlelisteServlet")
 public class handlelisteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+	VareListe vlist = new VareListe();
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession sesjon = request.getSession(false);
+		HttpSession s = request.getSession(false);
+		if(s != null) {
+			s.invalidate();
+		}else {
+			s = request.getSession(true);
+			s.setMaxInactiveInterval(30);
+			s.setAttribute("password", request.getParameter("PW"));
+			
+			
+			response.setContentType("text/html; charset=ISO-8859-1");
+			PrintWriter out = response.getWriter();
+			
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h3>");
+			out.println("Min handleliste");
+			out.println("</h3>");
+			out.println("<form action = \"handlelisteServlet\" method = \"post\">");
+			out.println("<p>");
+			out.println("<button> leggtil</button>");
+			out.println("<input type=\"text\" name=\"vare\"/>");
+			out.println("</p>");
+			out.println("</form>");
+			out.println("</body>");
+			out.println("</html>");
+			s.setAttribute("vare", request.getParameter("vare"));
+			vlist = (VareListe) s.getAttribute("vare");
 		
-//		if(sesjon == null ||)
 		
 		
 		
+		}
 		
-		response.setContentType("text/html; charset=ISO-8859-1");
-		PrintWriter out = response.getWriter();
 		
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h3>");
-		out.println("Min handleliste");
-		out.println("</h3>");
-		out.println("<form>");
-		out.println("<p>");
-		out.println("<button> leggtil</button>");
-		out.println("<input type=\"text\" name=\"vare\"/>");
-		out.println("</p>");
-		out.println("</form>");
-		out.println("</body>");
-		out.println("</html>");
+		
 		
 	}
 

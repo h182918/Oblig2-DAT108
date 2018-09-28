3bb8c437e7070df1c9a23bce5f2da9b3df1c128f
Torkel <182918@stud.hvl.no>
2018-09-28 14:14:27 +0200

    loginServlet 1

--- src/no/hvl/dat108/logInServlet.java
+++ src/no/hvl/dat108/logInServlet.java
@@ -8,34 +8,74 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class logInServlet
  */
-@WebServlet("/")
+@WebServlet("/login")
 public class logInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	String PW = null;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		
+		String rPW = request.getParameter("PW");
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>");
+		out.println("<form action=\"login\" method=\"post\">");
+		out.println("<fieldset>");
+		out.println("<legend>Login</legend>");
+		out.println("<h3>");
 		out.println("Oppgi passord");
-		out.println("</h1>");
-		out.println("<form action=\"myServlet\" method=\"get\">");
-		out.println("<p>Oppgi passord:");
-        out.println("<input type=\"text\" name=\"PW\" />");
+		out.println("</h3>");
+		out.println("<p>");
+        out.println("<input type=\"text\" name=\"PW\"/>");
         out.println("</p>");
 		out.println("<p><input type=\"submit\" value=\"Send data\" /></p>");
+		out.println("</fieldset>");
 		out.println("<form>");
 		out.println("</body>");
 		out.println("</html>");
+
+		if(!rPW.equals(PW)) {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<p>");
+			out.println("Du oppga feil passord");
+	        out.println("</p>");
+			out.println("</body>");
+			out.println("</html>");
+		}else {
+			HttpSession s = request.getSession(false);
+			
+			if(s != null) {
+				s.invalidate();
+			}else {
+				s = request.getSession(true);
+				s.setMaxInactiveInterval(30);
+				s.setAttribute("password", request.getParameter("PW"));
+			}
+		}
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		HttpSession s = request.getSession(false);
+		
+		if(s != null) {
+			s.invalidate();
+		}else {
+			s = request.getSession(true);
+			s.setMaxInactiveInterval(30);
+			s.setAttribute("password", request.getParameter("PW"));
+		}
+		
+		response.sendRedirect("handlelisteServlet");
+	}
+	
+	public void init() throws ServletException {
+		PW = "1234";
 	}
 
 }
